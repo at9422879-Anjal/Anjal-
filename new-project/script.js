45c38424c58c67506b8756b87a69e9d7a14419a1
Anjali <at9422879@gmail.com>
2026-01-20 16:53:57 +0530

    Update script.js

--- new-project/script.js
+++ new-project/script.js
@@ -1,1 +1,28 @@
+/* Dark Mode */
+function toggleDark() {
+  document.body.classList.toggle("dark");
+}
 
+/* Counter */
+let count = 0;
+
+function increase() {
+  count++;
+  document.getElementById("count").innerText = count;
+}
+
+function decrease() {
+  count--;
+  document.getElementById("count").innerText = count;
+}
+
+/* Image Change */
+function changeImg(num) {
+  let img = document.getElementById("img");
+
+  if (num === 1) {
+    img.src = "img/img1.jpg";
+  } else if (num === 2) {
+    img.src = "img/img2.jpg";
+  }
+}
